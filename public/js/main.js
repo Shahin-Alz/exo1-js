97a84a5155642e1798c0f4b1c28bb75c1f0e81fb
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-19 11:41:15 +0100

    finish

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+// creation
+let string;
+let number;
+let presentation;
+
+// affecter
+string = `Shahin`;
+number = 22
+presentation =`Bonjour je m'appel ${string} et j'ai ${number}ans`;
+
+// afficher
+console.log(string);
+console.log(number);
+console.log(presentation);
